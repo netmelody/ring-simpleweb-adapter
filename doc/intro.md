b836a7924268b07d6bb464f537f652d7dea9cfc2
scarytom <t.denley@cantab.net>
2012-11-06 22:35:18 +0000

    initial commit of lein generated code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ring-simpleweb-adapter
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/ring_simpleweb_adapter/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-simpleweb-adapter.core-test
+  (:use clojure.test
+        ring-simpleweb-adapter.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
