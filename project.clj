e2cad29dec994cbe0ed92e98ba578c3c6f99ed6a
scarytom <t.denley@cantab.net>
2012-11-06 22:48:18 +0000

    tidy up project.clj, and express dependencies on ring-core and simpleweb

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject ring-simpleweb-adapter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ring adapter for the simpleframwork lightweight server"
+  :url "http://github.com/netmelody/ring-simpleweb-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[ring/ring-core "1.1.6"]
+                 [org.simpleframework/simple "4.1.21"]]
+  :profiles
+  {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
