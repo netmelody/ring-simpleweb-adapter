56a3b49d6f5028c06e46ae4b87f1e4a9f8ae3989
Tom Denley <tom.denley@youdevise.com>
2012-11-07 12:26:09 +0000

    start unpicking the request map

--- src/ring/adapter/simpleweb.clj
+++ src/ring/adapter/simpleweb.clj
@@ -7,21 +7,37 @@
            (java.io PrintStream)))
 
 (defn build-request-map [request]
-  {})
+  {
+;   :remote-addr (-> request .getClientAddress .getHostAddress)
+;   :server-port        (.getPort uri)
+;   :server-name        (.getHost uri)
+   :uri                (-> request .getPath .toString)
+   :query-string       (-> request .getQuery .toString)
+   :request-method     (-> request .getMethod .toLowerCase keyword)
+;   :headers            {}
+   :content-type       (-> request .getContentType str)
+   :content-length     (-> request .getContentLength)
+;   :character-encoding (-> request .getContentType .getCharset)
+   :body               (-> request .getContent)
+   })
 
-(defn write-response [response-map ^Response response]
-  )
+(defn write-response [^Response response response-map]
+  (let [body (.getPrintStream response)]
+    (.set response "Content-Type", "text/plain")
+    (.set response "Server", "HelloWorld/1.0 (Simple 4.0)")
+    (.print body "Hello World")
+    (.close body)))
 
 (defn- proxy-handler
   "Returns a SimpleWeb Container implementation for the given Ring handler."
   [handler]
   (proxy [Container] []
     (handle [^Request request ^Response response]
-            (let [request-map (build-request-map request)
-                  response-map (handler request-map)]
-              (when response-map
-                (write-response response response-map))
-              (.close response)))))
+      (let [request-map (build-request-map request)
+            response-map (handler request-map)]
+        (when response-map
+          (write-response response response-map))
+        (.close response)))))
 
 (defn ^Connection run-simpleweb
   "Start a simpleframework web server to serve the given handler according to the supplied options:
